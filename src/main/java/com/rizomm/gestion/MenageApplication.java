1a4eb89b7a95fe05ec0e19353083838e7b2c9719
Romain DICK <r.dick.pro@gmail.com>
2020-03-18 11:44:02 +0100

    update : name Application

--- src/main/java/com/rizomm/gestion/MenageApplication.java
+++ src/main/java/com/rizomm/gestion/MenageApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GestionApplication {
+public class MenageApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GestionApplication.class, args);
+        SpringApplication.run(MenageApplication.class, args);
     }
 
 }
